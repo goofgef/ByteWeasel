8c3604f88f1100758696cf4c1083738931d170ec
AJ <hoi4useridkwhy1@gmail.com>
2026-03-06 20:47:53 -0600

    Flags added to main.c

--- src/main.c
+++ src/main.c
@@ -5,31 +5,44 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <string.h>
 
-int main() {
-    VM vm = {0};
-    init_vm(&vm, 2);
+int main(int argc, char *argv[]) {
+    if (argc >= 3) {
+        fprintf(stderr, "Usage: byteweasel -w/--what\n");
+        return 1;
+    }
 
-    vm.vtable->register_handler(&vm, 0, &add);
-    vm.vtable->register_handler(&vm, 1, &sub);
+    if (argc > 1 && (strcmp(argv[1], "--what") == 0 || strcmp(argv[1], "-w") == 0)) {
+        printf("ByteWeasel is a register-based VM made in C.\n");
+        printf("ByteWeasel is meant to be a framework that others can build on.\n");
+        return 0;
+    }else{
+        VM vm = {0};
+        init_vm(&vm, 2);
 
-    Instruction* bytecode = malloc(2 * sizeof(Instruction));
+        vm.vtable->register_handler(&vm, 0, &add);
+        vm.vtable->register_handler(&vm, 1, &sub);
 
-    int64_t ops0[] = {0, 1, 2};
-    int64_t ops1[] = {1, 1, 2};
+        Instruction* bytecode = malloc(2 * sizeof(Instruction));
 
-    bytecode[0] = vm.vtable->make(0, 3, ops0);
-    bytecode[1] = vm.vtable->make(1, 3, ops1);
+        int64_t ops0[] = {0, 1, 2};
+        int64_t ops1[] = {1, 1, 2};
 
-    vm.regs[1].data.value = 11;
-    vm.regs[2].data.value = 7;
+        bytecode[0] = vm.vtable->make(0, 3, ops0);
+        bytecode[1] = vm.vtable->make(1, 3, ops1);
 
-    vm.vtable->write(&vm, bytecode, 2);
-    vm.vtable->run(&vm);
+        vm.regs[1].data.value = 11;
+        vm.regs[2].data.value = 7;
 
-    printf("Result in r0: %" PRId64 "\n", vm.regs[0].data.value); //18
-    printf("Result in r1: %" PRId64 "\n", vm.regs[1].data.value); //4
+        vm.vtable->write(&vm, bytecode, 2);
+        vm.vtable->run(&vm);
 
-    vm.vtable->clean(&vm);
+        printf("Result in r0: %" PRId64 "\n", vm.regs[0].data.value); //18
+        printf("Result in r1: %" PRId64 "\n", vm.regs[1].data.value); //4
+
+        vm.vtable->clean(&vm);
+        return 0;
+    }
     return 0;
 }
